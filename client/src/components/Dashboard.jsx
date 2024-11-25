dc01f094c5957b1e6be7335923d0fb423a5ba3c0
Shri Krishna <skris56@uic.edu>
2024-11-24 22:06:22 -0600

    Update Dashboard component and add new songs to metadata

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -72,7 +72,7 @@ export default function Dashboard() {
     <Loader />
   ) : (
     <>
-      {allSongs && currentSong && (
+      {allSongs && (
         <div
           className="dashboard"
           style={{
