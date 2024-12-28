42c9c2973ddf3f04c0470f3cb83809cf04803d56
Shri Krishna <skris56@uic.edu>
2024-12-27 19:12:14 -0600

    fix: remove hardcoded MongoDB connection string from index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,8 +6,6 @@ import * as compression from "compression";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const mongoUrl =
-  "mongodb+srv://skris56:rhythpic%40123@rhythpic.0wk5z.mongodb.net/lycris?retryWrites=true&w=majority&appName=rhythpic";
 // const mongoUrl = "mongodb://localhost:27017/lycris";
 
 // Increase the limit for JSON payloads
